7502bab971c741fed81c2a4d0dbbfcf99834e429
Evan <evanburleigh24@gmail.com>
2025-09-19 17:19:46 -0400

    Add Gallery, Update some Styling for Uniformity across the site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Trips } from './trips/trips';
+import { Gallery } from './gallery/gallery';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: Home },
-    {path: 'trips', component: Trips}
+    {path: 'trips', component: Trips},
+    {path: 'gallery', component: Gallery}
 ];
---
+++ src/app/gallery/gallery.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  imports: [CommonModule],
+  templateUrl: './gallery.html',
+  styleUrl: './gallery.css'
+})
+export class Gallery {
+  isTallImg = signal<boolean | null>(false);
+  selectedImage = signal<string | null>(null);
+  selectedTitle = signal<string | null>(null);
+
+  openImage(img: string, title: string) {
+    this.selectedImage.set(img);
+    this.selectedTitle.set(title);
+
+    if(img === 'gallery21.jpg' || img === 'gallery14.jpg' || img === 'gallery20.jpg' || img === 'gallery5.jpg' || img === 'gallery7.jpg' || img === 'gallery11.jpg') {
+      this.isTallImg.set(true);
+    }
+    else {
+      this.isTallImg.set(false);
+    }
+  }
+
+  closeImage() {
+    this.selectedImage.set(null);
+    this.selectedTitle.set(null);
+  }
+}
